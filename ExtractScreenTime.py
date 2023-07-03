e23c4267f4d701c69e5c585a3786d650fe9a27db
Sushant Gautam <susant.gautam@gmail.com>
2023-07-03 11:34:19 +0200

    update and clean code to get game replay clips

--- ExtractScreenTime.py
+++ ExtractScreenTime.py
@@ -1,125 +1,56 @@
 import json
+import os
+import time
 
-import os# clear screen
-os.system('cls' if os.name == 'nt' else 'clear')
-def extract_common_labels(annotations):
-    common_labels = []
-    replay_labels = []
+def extract_screen_time_from_json(file_path, calcDiff=False):
+    with open(file_path, 'r') as file:
+        annotations = json.load(file)['annotations']
 
-    for annotation in annotations:
-        print("annotation", annotation)
-        # change_type = annotation['change_type']
-        replay = annotation.get('replay', '')
-        print("replay", replay, "replay_labels", replay_labels)
-        if replay == 'replay':
-            if replay_labels and any([label['link']['time']!= annotation['link']['time'] for label in replay_labels]):
-                replay_label = replay_labels[0]
-                common_labels.append({
-                    'label': replay_label,
-                    'startTime': replay_labels[0]['gameTime'],
-                    'endTime': annotation['gameTime']
-                })
-                replay_labels = []
-                replay_labels.append(annotation)
+    events = []
+    recording, followUp = False, False
+    for event in reversed(annotations):
+        if not recording and event['replay'] == 'replay':
+            recording = True
+            gameTime = f"{event['link']['half']} - {event['link']['time']}"
+            endTime = event['gameTime']
+            eventName = event['link']['label']
+        elif recording:
+            if event['replay'] == 'real-time':
+                recording = False
             else:
-                replay_labels.append(annotation)
-        elif replay == 'real-time' and replay_labels and all([label['link']['time']== replay_labels[0]['link']['time'] for label in replay_labels]):
-            replay_label = replay_labels[0]
-            common_labels.append({
-                'label': replay_label,
-                'startTime': replay_labels[0]['gameTime'],
-                'endTime': annotation['gameTime']
-            })
-            replay_labels = []
+                followUp = True
+            startTime = event['gameTime']
+            all_events = {
+                'gameTime': gameTime,
+                'startTime': startTime,
+                'endTime': endTime,
+                'followUp': followUp,
+                'eventName': eventName
+            }
+            events.append(all_events)
+            followUp = False
 
-    return common_labels
+    events = events[::-1]
+    for cr_evt, nx_evt in zip(events, events[1:]):
+        if cr_evt['gameTime'] == nx_evt['gameTime'] and cr_evt['eventName'] == nx_evt['eventName'] and cr_evt['endTime'] == nx_evt['endTime']:
+            cr_evt['endTime'] = nx_evt['startTime']
 
-# Example usage
-# Assuming your dataset is stored in a file called 'dataset.json'
-with open('./SoccerNet/england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1 Burnley/Labels-cameras.json', 'r') as file:
-    dataset = json.load(file)['annotations']
+    for event in events:
+        end_time = list(map(int, event['endTime'].replace('-', ':').split(':')))
+        start_time = list(map(int, event['startTime'].replace('-', ':').split(':')))
+        game_time = list(map(int, event['gameTime'].replace('-', ':').split(':')))
+        time_diff = (end_time[1] - start_time[1]) * 60 + (end_time[2] - start_time[2])
+        replay_delay =(start_time[1] - game_time[1]) * 60 + (start_time[2] - game_time[2])
 
-# common_labels = extract_common_labels(dataset)
-
-# parsed_common_labels = []
-# for label in common_labels:
-#     endTime = label['endTime']
-#     startTime = label['startTime']
-#     parsed_common_labels.append({
-#         'label': label['label']['link']['label'],
-#         'eventTime': label['label']['link']['time'],
-#         'startTime': label['startTime'],
-#         'endTime': endTime,
-#         # subtracting startTime from endTime to get the time difference
-#         'timeDiff': (int(endTime.split('-')[0]) - int(startTime.split('-')[0])) * 45 * 60 + (int(endTime.split('-')[1].split(':')[0]) - int(startTime.split('-')[1].split(':')[0])) * 60 + (int(endTime.split('-')[1].split(':')[1]) - int(startTime.split('-')[1].split(':')[1]))
-#     })
-
-
-# # Print the extracted common labels in single line
-# for label in parsed_common_labels:
-#     print(
-#         label['label'], "-", label['eventTime'],
-#            "\t\tstartTime", label['startTime'], "\t\tendTime", label['endTime'], "\t\ttimeDiff", label['timeDiff'])
-
-
-# invert dataset
-
-recording = False
-
-gameTime, startTime, endTime, eventName, followUp = None, None, None, None, False    
-all_events=[]
-
-for events in dataset[::-1]:
-    if recording ==False and events['replay'] == 'replay':
-        recording = True
-        gameTime=events['link']['half']+" - "+ events['link']['time'] #sample:  '2- 23:09', time
-        endTime = events['gameTime']
-        eventName = events['link']['label']
-
-    elif recording ==True:
-        if events['replay'] == "real-time":
-            recording = False
+        if calcDiff:
+            event.update({'timeDiff': time_diff, 'replayDelay': replay_delay})
             
-        else:
-            followUp = True
-            
-        startTime = events['gameTime'] # sample: 'startTime': '2 - 17:05',, gameHalf - time
-        all_events.append({"gameTime": gameTime, "startTime": startTime, "endTime": endTime,
-                        #    "timeDiff": timeDiff,
-                        #    "replayDelay": replayDelay,                            
-                           "followUp": followUp,
-                           "eventName": eventName, 
-                           })
-        followUp = False
+        event['eventName']= event.pop('eventName')
+    return events
 
 
-events= all_events[::-1]
-
-# if gameTime, eventName and endTime is same as the next event in events, then set its endTime to the next event's startTime
-for i in range(len(events) - 1):
-    current_event = events[i]
-    next_event = events[i + 1]
-
-    if (
-        current_event["gameTime"] == next_event["gameTime"]
-        and current_event["eventName"] == next_event["eventName"]
-        and current_event["endTime"] == next_event["endTime"]
-    ):
-        current_event["endTime"] = next_event["startTime"]
-
-
-# add timeDiff and replayDelay
-for i in range(len(events)):
-    #diff between start and end time
-    timeDiff= (int(events[i]['endTime'].split('-')[0]) - int(events[i]['startTime'].split('-')[0])) * 45 * 60 + (int(events[i]['endTime'].split('-')[1].split(':')[0]) - int(events[i]['startTime'].split('-')[1].split(':')[0])) * 60 + (int(events[i]['endTime'].split('-')[1].split(':')[1]) - int(events[i]['startTime'].split('-')[1].split(':')[1]))
-
-    # Calculate the difference between gameTime and startTime
-    replayDelay = (int(events[i]['startTime'].split('-')[0]) - int(events[i]['gameTime'].split('-')[0])) * 45 * 60 + (int(events[i]['startTime'].split('-')[1].split(':')[0]) - int(events[i]['gameTime'].split('-')[1].split(':')[0])) * 60 + (int(events[i]['startTime'].split('-')[1].split(':')[1]) - int(events[i]['gameTime'].split('-')[1].split(':')[1]))
-
-    events[i]['timeDiff'] = timeDiff
-    events[i]['replayDelay'] = replayDelay
-    # make eventName key at last
-    events[i]['eventName'] = events[i].pop('eventName')
+file_path = './SoccerNet/england_epl/2014-2015/2015-02-21 - 18-00 Chelsea 1 - 1 Burnley/Labels-cameras.json'
+events = extract_screen_time_from_json(file_path, calcDiff=True)
 
 for event in events:
     print(event)
